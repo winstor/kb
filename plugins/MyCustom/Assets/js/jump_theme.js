35ebb35273b2c004a033ac6da4455c9b4ad00517
winstor <540124900@qq.com>
2020-10-15 11:54:08 +0800

    init

--- plugins/MyCustom/Assets/js/jump_theme.js
+++ plugins/MyCustom/Assets/js/jump_theme.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     //概况view-overview 看板view-board  列表view-listing  日程表view-calendar 甘特图view-gantt
     $("body").on("click", "a", function () {
-        if(check_jump_function($(this))){
+        if (check_jump_function($(this))) {
             $("#jump_load").remove();
             $('body').append('<div id="jump_load" style="height: 4px;width: 0;background: green;position: absolute;top:75px;z-index:99999"></div>');
             $("#jump_load").animate({width: "50%"}, 1000, function () {
@@ -9,17 +9,27 @@ $(document).ready(function () {
             });
         }
     });
-    function check_jump_function(obj)
-    {
+
+    function check_jump_function(obj) {
         var href = obj.attr("href");
         var target = obj.attr("target");
         //过滤选择跳转
-        if(obj.hasClass("filter-helper")){
-            return true;
-        }else if(!href || href === "#" || target === "_blank"){
+        if (target === "_blank") {
             return false;
-        }else if(obj.attr("class").indexOf("js-modal") === -1){
+        }
+        if (obj.hasClass("filter-helper")) {
             return true;
+        }
+        if (obj.hasClass("js-subtask-toggle-status")) {
+            return false;
+        }
+        if (href && href !== "#" ) {
+            var check_modal = obj.attr("class").indexOf("js-modal");
+            if(href.indexOf("FileViewerController") !== -1){
+                return false;
+            }else if(check_modal === -1){
+                return true;
+            }
         }
         return false;
     }
