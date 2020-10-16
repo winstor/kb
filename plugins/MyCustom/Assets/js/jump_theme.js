06f82a114cd295b6da1a38e0334107a89686ce0b
winstor <540124900@qq.com>
2020-10-16 14:20:48 +0800

    init

--- plugins/MyCustom/Assets/js/jump_theme.js
+++ plugins/MyCustom/Assets/js/jump_theme.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
     //概况view-overview 看板view-board  列表view-listing  日程表view-calendar 甘特图view-gantt
     $("body").on("click", "a", function () {
+        var jump_load_out;
         if (check_jump_function($(this))) {
-            $("#jump_load").remove();
-            $('body').append('<div id="modal-blurpic" style="backdrop-filter: blur(0);background: rgba(0, 0, 0, 0);"></div><div id="jump_load" style="height: 4px;width: 0;background: green;position: absolute;top:75px;z-index:99999"></div>');
-            $("#jump_load").animate({width: "50%"}, 1000, function () {
-                $(this).animate({width: "100%"}, 20000);
-            });
+            jump_load_out = setTimeout(function(){
+                clearTimeout(jump_load_out);
+                $("#jump_load").remove();
+                $('body').append('<div id="modal-blurpic" style="backdrop-filter: blur(0);background: rgba(0, 0, 0, 0);"></div><div id="jump_load" style="height: 4px;width: 0;background: green;position: absolute;top:75px;z-index:99999"></div>');
+                $("#jump_load").animate({width: "50%"}, 1000, function () {
+                    $(this).animate({width: "100%"}, 20000);
+                });
+            },1000)
         }
     });
 
