6a8552dda539974e3ff33f08fe9f6ab8532ffe96
winstor <540124900@qq.com>
2020-10-16 17:59:18 +0800

    init

--- plugins/MyCustom/Assets/js/jump_theme.js
+++ plugins/MyCustom/Assets/js/jump_theme.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
                         $(this).animate({width: "97%"}, 50000);
                     });
                 });
-            },500)
+            },200)
         }
     });
 
